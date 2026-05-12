8f41c9d8a1abcb88a721cb50d3f867861e6669d7
UncleSubak <kko6369@naver.com>
2026-05-12 12:21:04 +0900

    chore: 가게 모듈 구조 생성 및 의존성 설정

---
+++ store-api/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.kotlin.jpa)
+}
+
+dependencies {
+    implementation(project(":common"))
+
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.tsid.creator)
+
+    compileOnly(libs.springdoc.openapi.webmvc.ui)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.boot.starter.data.jpa.test)
+    testImplementation(libs.spring.boot.testcontainers)
+    testImplementation(libs.testcontainers.mysql)
+    testImplementation(libs.testcontainers.junit.jupiter)
+}
